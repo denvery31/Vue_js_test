160112a51f201baa2f7425fc3c1fe07d1bf2d269
denvery31 <denvery31@gmail.com>
2022-03-30 22:29:39 +0300

    v-for testing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
   <p>Hello</p>
+  <div>
+    <div v-for="player in palyers" :key="player">
+      {{ player.name }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+
+  data() {
+    return {
+      palyers: [
+        {
+          name: "kolya",
+          age: 25,
+        },
+        {
+          name: "petya",
+          age: 20,
+        },
+        {
+          name: "vasya",
+          age: 30,
+        },
+      ],
+    };
+  },
+
+  methods: {},
 };
 </script>
 
