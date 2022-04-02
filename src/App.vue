6eab82cf995917b70690016ac91e30717fa59c90
denvery31 <denvery31@gmail.com>
2022-04-02 18:12:14 +0300

    addedd more css || button prev

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <p>Wating for other playes...</p>
+  <p class="headder">Wating for other playes...</p>
   <div class="p_block">
     <p class="p_top">Palyers:</p>
     <div class="p_playres">
       <div v-for="player in palyers" :key="player.name" class="player">
         <p>{{ player.name }}</p>
+        <p>Ready: {{ player.ready }}</p>
       </div>
     </div>
-    <button>Start</button>
+    <div class="btn">
+      <button
+        @click="
+          this.palyers[this.your_id].ready
+            ? make_all_not_ready()
+            : make_all_ready()
+        "
+        class="btn_ready"
+      >
+        {{ this.palyers[this.your_id].ready ? "Canel" : "Ready" }}
+      </button>
+      <button v-if="!each_player_ready()" class="btn_exit">Exit</button>
+      <button v-if="each_player_ready()" class="btn_start">Start</button>
+    </div>
   </div>
 </template>
 
@@ -17,28 +31,52 @@ export default {
 
   data() {
     return {
+      your_id: 1,
       palyers: [
         {
           name: "Kolya",
           age: 25,
+          ready: false,
         },
         {
           name: "Petya",
           age: 20,
+          ready: false,
         },
         {
           name: "Vasya",
           age: 30,
+          ready: false,
         },
         {
           name: "Dmitriy",
           age: 12,
+          ready: false,
         },
       ],
     };
   },
 
-  methods: {},
+  methods: {
+    each_player_ready() {
+      for (let player of this.palyers) {
+        if (!player.ready) {
+          return false;
+        }
+      }
+      return true;
+    },
+    make_all_ready() {
+      this.palyers.forEach((player) => {
+        player.ready = true;
+      });
+    },
+    make_all_not_ready() {
+      this.palyers.forEach((player) => {
+        player.ready = false;
+      });
+    },
+  },
 };
 </script>
 
@@ -47,6 +85,11 @@ export default {
 body {
   font-family: "Noto Sans", sans-serif;
   font-size: 2.4rem;
+  background-color: rgb(245, 242, 239);
+}
+
+.headder {
+  text-align: center;
 }
 
 .p_playres {
@@ -60,6 +103,7 @@ body {
 
 .p_playres > .player {
   border-bottom: 0.1rem solid rgb(52, 129, 35);
+  display: flex;
 }
 
 .p_top {
@@ -76,5 +120,21 @@ body {
 
   border: 0.3rem solid rgb(168, 212, 73);
   border-radius: 35px;
+
+  background-color: rgba(255, 255, 255, 0.496);
+}
+
+.btn button {
+  border: none;
+  padding: 2%;
+  border-radius: 12px;
+  width: 15%;
+  margin: 0 3%;
+  background-color: rgb(227, 213, 194);
+}
+
+.btn button:hover {
+  opacity: 80%;
+  cursor: pointer;
 }
 </style>
