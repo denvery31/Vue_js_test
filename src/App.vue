e240886b927a6985e3244b3a6318583b533d7fa5
denvery31 <denvery31@gmail.com>
2022-04-02 12:53:51 +0300

    addded css 2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <p>Hello</p>
+  <p>Wating for other playes...</p>
   <div class="p_block">
-    <div v-for="player in palyers" :key="player.name" class="player">
-      {{ player.name }}
+    <p class="p_top">Palyers:</p>
+    <div class="p_playres">
+      <div v-for="player in palyers" :key="player.name" class="player">
+        <p>{{ player.name }}</p>
+      </div>
     </div>
+    <button>Start</button>
   </div>
 </template>
 
@@ -15,16 +19,20 @@ export default {
     return {
       palyers: [
         {
-          name: "kolya",
+          name: "Kolya",
           age: 25,
         },
         {
-          name: "petya",
+          name: "Petya",
           age: 20,
         },
         {
-          name: "vasya",
+          name: "Vasya",
           age: 30,
+        },
+        {
+          name: "Dmitriy",
+          age: 12,
         },
       ],
     };
@@ -35,10 +43,38 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap");
+body {
+  font-family: "Noto Sans", sans-serif;
+  font-size: 2.4rem;
+}
+
+.p_playres {
+  max-width: 100%;
+}
+
+.p_playres p {
+  max-width: 90%;
+  margin: auto;
+}
+
+.p_playres > .player {
+  border-bottom: 0.1rem solid rgb(52, 129, 35);
+}
+
+.p_top {
+  border-bottom: 0.2rem solid rgb(53, 70, 15);
+  margin: 0;
+  margin-bottom: 2%;
+}
+
 .p_block {
-  background-color: red;
-  padding: 10%;
-  margin: 7%;
+  padding: 3%;
+
+  max-width: 50%;
+  margin: auto;
+
+  border: 0.3rem solid rgb(168, 212, 73);
   border-radius: 35px;
 }
 </style>
